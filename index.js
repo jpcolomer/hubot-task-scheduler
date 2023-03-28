e6765680173e7c430efc489be67cdad9423ff063
Adi <adi@thenumber.com>
2023-03-28 11:15:46 -0400

    removed that extra space

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 var Scheduler = require('./lib/scheduler');
 
-module.exports = function (bot) {
+module.exports = function(bot) {
   /*
   Description:
     Hubot job scheduler
@@ -29,12 +29,12 @@ module.exports = function (bot) {
 
   scheduler().initializeScheduledJobs(bot);
 
-  bot.respond(/run job (.+)$/i, function (message) {
+  bot.respond(/run job (.+)$/i, function(message) {
     const ref = message.match.slice(1);
     const fn = ref[0];
     return scheduler().jobFunctions[fn](
       {
-        send: function (msg, channel = room) {
+        send: function(msg, channel = room) {
           sendMessage(bot, message, msg, channel);
         },
         bot
@@ -42,45 +42,45 @@ module.exports = function (bot) {
     );
   });
 
-  bot.respond(/delete scheduled job (.+)$/i, function (message) {
+  bot.respond(/delete scheduled job (.+)$/i, function(message) {
     const ref = message.match.slice(1);
     const fn = ref[0];
     return scheduler().deleteScheduledJob(bot, fn)
-      .then(function () {
+      .then(function() {
         return sendMessage(bot, message, "Deleted scheduled job " + fn);
       },
-        function (err) {
+        function(err) {
           return sendMessage(bot, message, "Failed deletion of scheduled job " + fn);
         });
   });
 
-  bot.respond(/schedule job (.+) [“”"'‘](.+)[“”"'’]$/i, function (message) {
+  bot.respond(/schedule job (.+) [“”"'‘](.+)[“”"'’]$/i, function(message) {
     const ref = message.match.slice(1);
     const fn = ref[0];
     const cronTime = ref[1];
     message.send("Scheduling Job");
     return scheduler().scheduleJob(bot, fn, cronTime)
-      .then(function () {
+      .then(function() {
         return scheduler().registerJob(fn, cronTime,
           {
-            send: function (msg, channel = room) {
+            send: function(msg, channel = room) {
               sendMessage(bot, message, msg, channel);
             },
             bot
           }
         );
       })
-      .then(function () {
+      .then(function() {
         return sendMessage(bot, message, `Scheduled ${fn} '${cronTime}'`);
       });
   });
 
-  bot.respond(/list scheduled jobs/i, function (message) {
+  bot.respond(/list scheduled jobs/i, function(message) {
     const brainScheduledJobs = bot.brain.get(scheduler().JOBS);
-    const list = Object.keys(brainScheduledJobs || {}).reduce(function (o, d) {
+    const list = Object.keys(brainScheduledJobs || {}).reduce(function(o, d) {
       o += '[' + d + ']: ' + brainScheduledJobs[d][1] + '\n';
       return o;
     }, '');
     sendMessage(bot, message, list.length && list || 'No scheduled jobs');
   });
-}
+};
